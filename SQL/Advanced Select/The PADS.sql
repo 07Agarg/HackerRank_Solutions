6885585cb786e72b1bc69800fdd2f890a9ff8beb
07Agarg <agarwal.ashima07@gmail.com>
2019-02-21 03:43:15 +0530

    Update SQL Solutions

--- SQL/Advanced Select/The PADS.sql
+++ SQL/Advanced Select/The PADS.sql
@@ -1,1 +1,4 @@
+SELECT concat(Name, '(', substring(Occupation, 1, 1), ')') FROM OCCUPATIONS ORDER BY NAME;
+
 SELECT concat('There are a total of ', count(*), ' ', LOWER(Occupation), 's.') AS TOTAL FROM OCCUPATIONS GROUP BY OCCUPATION ORDER BY TOTAL;
+
--- SQL/Basic Select/Weather Observation Station 12.sql
+++ SQL/Basic Select/Weather Observation Station 12.sql
@@ -1,6 +1,1 @@
-SELECT CASE WHEN A+B <= C OR B+C <= A OR A+C <= B THEN 'Not A Triangle'
-    WHEN A = B AND B = C THEN 'Equilateral'
-    WHEN A = B OR B = C OR A = C THEN 'Isosceles'
-    ELSE 'Scalene'
-    END
-FROM TRIANGLES;
+SELECT DISTINCT city FROM STATION WHERE NOT ((CITY LIKE '%a' or CITY LIKE '%e' or CITY LIKE '%i' or CITY LIKE '%o' or CITY LIKE '%u') OR (CITY LIKE 'a%' or CITY LIKE 'e%' or CITY LIKE 'i%' or CITY LIKE 'o%' or CITY LIKE 'u%'))
